a06726792b81c3bb385304b3621dee38b9f459cb
langhuihui <178529795@qq.com>
2021-06-27 22:30:18 +0800

    更新sponsor

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,23 @@
 <template>
     <div>
         <h1>
-            {{$t('about.title')}}
+            {{ $t("about.title") }}
         </h1>
         <p>
-            {{$t('about.desc')}}
+            {{ $t("about.desc") }}
         </p>
-        <div style=" background: lightgray;">
-            <img src="../assets/alipay.png">
-            <img src="../assets/wechat.jpg">
+        <div style="background: lightgray">
+            <img src="../assets/alipay.png" />
+            <img src="../assets/wechat.jpg" />
         </div>
-        <Divider> {{$t('about.sponsor')}}</Divider>
+        <Divider> {{ $t("about.sponsor") }}</Divider>
         <Tag color="cyan">Sense</Tag>
         <Tag color="cyan">hshev99</Tag>
         <Tag color="cyan">兵</Tag>
         <Tag color="cyan">Leon</Tag>
         <Tag color="cyan">Jack</Tag>
         <Tag color="cyan">天空</Tag>
+        <Tag color="cyan">李亚宁</Tag>
     </div>
 </template>
 <style scoped>
