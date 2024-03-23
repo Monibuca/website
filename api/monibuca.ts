ad6345eb129d948ef294d1e31d63b02db7641a50
dexter <178529795@qq.com>
2024-03-23 21:38:45 +0800

    fix: v5 go module

--- api/monibuca.ts
+++ api/monibuca.ts
@@ -2,14 +2,14 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (req: VercelRequest, res: VercelResponse) {
   const url = new URL(req.url!, `http://${req.headers.host}`);
-  const [, , monibuca, version] = url.pathname.split('/');
+  const [, monibuca, version] = url.pathname.split('/');
   res.end(`<!DOCTYPE html>
 <html>
 
 <head>
-  <meta name="go-import" content="m7s.live/${monibuca}/${version} git ssh://git@github.com/langhuihui/${monibuca}">
+  <meta name="go-import" content="m7s.live/${monibuca}/${version} git ssh://git@github.com/langhuihui/monibuca">
   <meta name="go-source"
-    content="m7s.live/${monibuca}/${version} _ https://github.com/langhuihui/${monibuca}/tree/${version}{/dir} https://github.com/langhuihui/${monibuca}/tree/${version}{/dir}/{file}#L{line}">
+    content="m7s.live/${monibuca}/${version} _ https://github.com/langhuihui/monibuca/tree/${version}{/dir} https://github.com/langhuihui/monibuca/tree/${version}{/dir}/{file}#L{line}">
   <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/m7s.live/${monibuca}/${version}">
 </head>
 
